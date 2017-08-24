a123493572a803b665b9770e4a0390fb254383be
Jiawei Zhuang <zhuangjw@Jiaweis-MacBook-Pro.local>
2017-08-23 21:00:09 -0400

    Initial commit
    Signed-off-by: Jiawei Zhuang <zhuangjw@Jiaweis-MacBook-Pro.local>

---
+++ dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# KPP environment on ubuntu
+sudo apt-get update
+sudo apt-get install build-essential #make, GCC
+sudo apt-get install flex bison
+
+# Run the generated fortran code
+sudo apt-get install gfortran 
